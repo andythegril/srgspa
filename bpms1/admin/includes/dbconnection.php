c0f8221330407909c2ac1dec88c34b689e4b44e5
andythegril <95900562+andythegril@users.noreply.github.com>
2022-07-14 18:03:37 +0700

    Update dbconnection.php

--- bpms1/admin/includes/dbconnection.php
+++ bpms1/admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("sql312.epizy.com", "epiz_32138520", "UJcHFedussTm9", "epiz_32138520_bpmsdb");
+$con=mysqli_connect("localhost", "root", "", "bpmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
